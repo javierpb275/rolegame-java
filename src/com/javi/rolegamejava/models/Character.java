85d7e6980ba4af529258f250080367ee0c91b878
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-08 16:17:37 +0200

    created attributes for the character class

--- src/com/javi/rolegamejava/models/Character.java
+++ src/com/javi/rolegamejava/models/Character.java
@@ -2,5 +2,12 @@ package com.javi.rolegamejava.models;
 
 //This is the parent class, which extends from User (grandparent class)
 public class Character extends User {
+	
+	//protected attributes so that its child can have access to them.
+	protected String name;
+	protected String race;
+	protected int damage;
+	protected int defense;
+	
 
 }
