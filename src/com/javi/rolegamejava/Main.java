7b636f9378cecb48209dffea82554f860905a743
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-08 16:03:17 +0200

    created new user and checks if we can print their info

--- src/com/javi/rolegamejava/Main.java
+++ src/com/javi/rolegamejava/Main.java
@@ -1,10 +1,16 @@
 package com.javi.rolegamejava;
 
+import com.javi.rolegamejava.models.User;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		//create new user
+		User user1 = new User("pepe", "pepe@gmail.com", "123");
+		
+		//checking if we can print the user info:
+		System.out.println("I am " + user1.getUsername() + ". My email is " + user1.getEmail() + ". My password is " + user1.getPassword());
 
 	}
 
