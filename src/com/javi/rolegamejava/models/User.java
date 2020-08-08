d0f1d1e85efd659e3c61bdbdb728983b5d28b436
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-08 15:24:34 +0200

    added some comments for what we added before

--- src/com/javi/rolegamejava/models/User.java
+++ src/com/javi/rolegamejava/models/User.java
@@ -1,7 +1,9 @@
 package com.javi.rolegamejava.models;
 
+//This is the grandparent class, which is the user that registers and signs in.
 public class User {
 	
+	//private attributes since only user can have access to them.
 	private String username;
 	private String email;
 	private String password;
