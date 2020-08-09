00daee6f27975d7119ed0d3d202be0ddf8c28270
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-09 21:34:33 +0200

    added Scanner to class to Main and created attributes for User

--- src/com/javi/rolegamejava/Main.java
+++ src/com/javi/rolegamejava/Main.java
@@ -1,10 +1,15 @@
 package com.javi.rolegamejava;
 
+import java.util.Scanner;
+
 import com.javi.rolegamejava.models.User;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		//This the Scanner class to be able to type info in the console
+		Scanner s = new Scanner(System.in);
 		
 		//create new user
 		User user1 = new User("pepe", "pepe@gmail.com", "123");
