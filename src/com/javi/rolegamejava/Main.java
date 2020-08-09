f4a3629b60378de513de50a2e028026098572616
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-09 21:50:32 +0200

    created user using the scanner class and checked if we get their info

--- src/com/javi/rolegamejava/Main.java
+++ src/com/javi/rolegamejava/Main.java
@@ -8,14 +8,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		 String username;
+		 String email;
+		 String password;
+		
 		//This the Scanner class to be able to type info in the console
 		Scanner s = new Scanner(System.in);
 		
+		//We ask the user info:
+		System.out.println("Please, enter your username:");
+		username = s.nextLine();
+		
+		System.out.println("Please, enter your email:");
+		email = s.nextLine();
+		
+		System.out.println("Please, enter your password:");
+		password = s.nextLine();
+		
 		//create new user
-		User user1 = new User("pepe", "pepe@gmail.com", "123");
+		User user = new User(username, email, password);
 		
 		//checking if we can print the user info:
-		System.out.println("I am " + user1.getUsername() + ". My email is " + user1.getEmail() + ". My password is " + user1.getPassword());
+		System.out.println("I am " + user.getUsername() + ". My email is " + user.getEmail() + ". My password is " + user.getPassword());
 
 	}
 
